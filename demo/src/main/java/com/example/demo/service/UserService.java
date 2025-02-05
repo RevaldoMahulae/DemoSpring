377911a000a17d11de2806460eda08962ced56a8
MikhaelBilly <mikhael.billy@qualitas.co.id>
2025-02-05 09:18:00 +0700

    Update User Service

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -3,9 +3,11 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.User;
 import com.example.demo.util.ResponseWrapper;
+import com.example.demo.model.User;
 
 public interface UserService {
     List<User> getAllUsers();
     ResponseWrapper findUserById(Long id);
-    void saveUser(User user);
+
+    User saveUser(User user);
 }
